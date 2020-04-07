e7e9cd41bda34c411cd09a7a61cadb76a1d1275d
Andre Soares <andregsilv@gmail.com>
2020-04-07 00:16:44 -0300

    Add startup script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,6 @@
+# Install vim-plug for Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim +PlugInstall +qall
+nvim +GoInstallBinaries +qall
