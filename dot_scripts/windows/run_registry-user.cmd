be9c7d5ab055b79752a8504c1d05153bda302b22
Andre Soares <andregsilv@gmail.com>
2020-06-17 10:27:41 -0300

    Remove extended

--- dot_scripts/windows/run_registry-user.cmd
+++ dot_scripts/windows/run_registry-user.cmd
@@ -19,7 +19,6 @@ reg add "HKCU\Control Panel\Accessibility\Keyboard Response" /t REG_DWORD /v Fla
 reg add "HKCU\Control Panel\Accessibility\StickyKeys"        /t REG_SZ    /v Flags         /d 26 /f 
 reg add "HKCU\Control Panel\Accessibility\ToggleKeys"        /t REG_SZ    /v Flags         /d 58 /f 
 
-reg add "HKCU\SOFTWARE\Classes\Directory\Background\shell\Open WT Powershell here"         /t REG_SZ        /v "Extended" /f 
 reg add "HKCU\SOFTWARE\Classes\Directory\Background\shell\Open WT Powershell here\command" /t REG_EXPAND_SZ /ve /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe\" new-tab -p \"{574e775e-4f2a-5b96-ac1e-a2962a402336}\" -d \"%%V\"" /f 
 
 reg add "HKCU\SOFTWARE\Classes\Directory\Background\shell\Open WT WSL here\command" /t REG_EXPAND_SZ /ve /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe\" new-tab -p \"{a5a97cb8-8961-5535-816d-772efe0c6a3f}\" -d \"%%V\"" /f 
