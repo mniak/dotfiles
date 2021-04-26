d359f0c91312674d93132d87980adaa421264b6d
Phantom <>
2021-04-26 18:35:00 -0300

    Disable powershell modules

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
-import-module posh-git
-import-module oh-my-posh
-set-theme Paradox
+# import-module posh-git
+# import-module oh-my-posh
+# set-theme Paradox
 
 set-alias -name vim -value nvim
 set-alias -name vi -value nvim
