6b34af6c456f6f4447847d714b90f0b4144b9404
Andre Soares <andregsilv@gmail.com>
2021-04-27 11:13:13 -0300

    disable cortana button

--- dot_scripts/windows/run_registry-user.bat
+++ dot_scripts/windows/run_registry-user.bat
@@ -1,10 +1,11 @@
 @echo off
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer"          /t REG_DWORD /v AllowedEnumeration /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v HideFileExt        /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v TaskbarSmallIcons  /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v ShowTaskViewButton /d 0 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v Hidden             /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v HideFileExt        /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v ShowCortanaButton  /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v ShowTaskViewButton /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /t REG_DWORD /v TaskbarSmallIcons  /d 1 /f
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /t REG_DWORD /v CortanaConsent       /d 0 /f 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /t REG_DWORD /v DeviceHistoryEnabled /d 0 /f
