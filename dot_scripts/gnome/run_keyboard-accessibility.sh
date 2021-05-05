d4596e0c38b39460902f1467a639ad3bbeaeab2f
Andre Soares <andregsilv@gmail.com>
2021-05-05 12:00:41 -0300

    fix acces;

--- dot_scripts/gnome/run_keyboard-accessibility.sh
+++ dot_scripts/gnome/run_keyboard-accessibility.sh
@@ -5,8 +5,8 @@ read -r -d '' CONTENT << DELIMITER-1234
 [org/gnome/desktop/peripherals/keyboard]
 delay=uint32 200
 repeat=true
-repeat-interval=uint32 20
+repeat-interval=uint32 25
 
 DELIMITER-1234
 
-echo "$CONTENT" | dconf load /org/gnome/desktop/
+echo "$CONTENT" | dconf load /
