3d81b25a561c8c678451f9249ecb06e81aaa18db
Andre Soares <andregsilv@gmail.com>
2021-05-06 17:41:08 -0300

    arc dark

--- dot_scripts/gnome/run_themes.sh
+++ dot_scripts/gnome/run_themes.sh
@@ -2,8 +2,8 @@
 
 read -r -d '' CONTENT << DELIMITER-1234
 [desktop/interface]
-cursor-theme='Arc-Dark-Solid'
-gtk-theme='Dracula'
+cursor-theme='Bibata-Modern-Ice'
+gtk-theme='Arc-Dark-solid'
 icon-theme='Arc'
 monospace-font-name='JetBrains Mono 10'
 DELIMITER-1234
