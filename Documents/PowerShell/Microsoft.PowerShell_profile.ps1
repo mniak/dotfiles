728b772f257c8d5e0745b86a2de0ce1c2b29ecc1
Andre Soares <andregsilv@gmail.com>
2021-05-06 13:39:57 -0300

    starship on powershell

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,5 @@
-# import-module posh-git
-# import-module oh-my-posh
-# set-theme Paradox
-
 set-alias -name vim -value nvim
 set-alias -name vi -value nvim
 set-alias -name sudo -value gsudo
+
+Invoke-Expression (&starship init powershell)
